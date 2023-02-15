5709e9d0371b1435b620b9bbf7b59149b2bd830a
Guilherme <guiartbp@gmail.com>
2023-02-15 16:44:15 -0300

    done

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ const gridItem = document.getElementsByClassName('grid-item');
 const corePalette = document.getElementById('color');
 const colorSelected = document.getElementById('colorSelected');
 const colorRainbow = document.getElementById('colorRainbow');
+const colorTen = document.getElementById('colorTen');
 const Eraser = document.getElementById('eraser');
 const clear = document.getElementById('clear');
 const gridSize = 500;
 const buttons = document.querySelectorAll('button');
+const inputSize = document.getElementById('inputSize');
+const colorP = document.getElementById('colorP');
 
-let color = 'black'
+
 let selectButton = 'colorSelected'
 
 
@@ -27,6 +30,7 @@ function removeGrid() {
 function makeGrid(size) {
     grid.style.setProperty('--grid-rows', size);
     grid.style.setProperty('--grid-cols', size);
+
     let square = size * size;
     for (let i = 0; i < square; i++) {
         let cell = document.createElement('div')
@@ -39,32 +43,37 @@ function makeGrid(size) {
         gridItem[i].style.width = gridSquare + "px"
         gridItem[i].style.height = gridSquare + "px";
     }    
+    inputSize.innerText = `${size} x ${size}`
 }
 
 function colorRandom() {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-
+    color.value = randomColor;
 }
 
 function changeColor(changeSquare) {
     if (selectButton === 'colorSelected' || selectButton === 'clear' ) {
-        gridItem[changeSquare].style.backgroundColor = color;
-        console.log(color)    
+        gridItem[changeSquare].style.backgroundColor = color; 
+        colorP.innerText = `${color}`     
     } else if (selectButton === 'colorRainbow') {
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
         gridItem[changeSquare].style.backgroundColor = "#" + randomColor;
-        console.log("#" + randomColor)    
-        console.log(selectButton)
-    } else if (selectButton === 'eraser') {
+        colorP.innerText = `#${randomColor}`     
+
+    } else if (selectButton === 'colorTen') {
+        gridItem[changeSquare].style.backgroundColor = "#" + randomColor;
+        colorP.innerText = `#${randomColor}`     
+
+    }else if (selectButton === 'eraser') {
         gridItem[changeSquare].style.backgroundColor = 'white';
-        console.log("white")    
-        console.log(selectButton)
+        colorP.innerText = `#FFFFFF`     
     }
 
 }
 grid.addEventListener('mouseover', function(e) {
     var selectSquare = e.target 
     color = corePalette.value
+
     changeColor(selectSquare.id)
 });
 
@@ -79,9 +88,18 @@ range.addEventListener('click', () => {
 });
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
+    
+    button.addEventListener('click', function () {
         selectButton = button.id
+        buttons.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+        if (button.id === 'clear'){
+            this.classList.remove('active');
+            buttons[0].classList.add('active')
+        }
     });
-  });
+    
+});
+
 
 makeGrid(range.value)
